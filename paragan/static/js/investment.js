46bfdf8eea8d2476bf80f2fa1fac385e735aa6c2
osimuka <osimukaokpan@gmail.com>
2024-04-27 02:03:09 +0100

    update

---
+++ paragan/static/js/investment.js
@@ -0,0 +1,37 @@
+// const moneyData = [
+//   {
+//     id: 1,
+//     initials: 10000,
+//     profit: initials / 10,
+//   },
+//   {
+//     id: 2,
+//     initials: 20000,
+//     profit: initials / 10,
+//   },
+// ];
+
+// const container = document.querySelector(".container");
+
+// moneyData.forEach((item) => {
+//   `<div class="invest-card">
+//             <img width="70" height="70" src="https://img.icons8.com/ultraviolet/40/sales-performance.png"
+//                 alt="sales-performance" />
+//             <div class="invest-card-capital">
+//                 <p>Capital</p>
+//                 <p>N${item.initials}</p>
+//             </div>
+//             <div class="invest-card-profit">
+//                 <p style="font-size: 0.9rem;">Daily Profit</p>
+//                 <p style="font-size: 1.5rem;">N${item.profit}</p>
+//             </div>
+//             <Link to="/payment">
+//             <button class="invest-btn">
+//                 <a href="{% url 'app:paystack' %}">Invest ₦</a>
+//             </button>
+//             </Link>
+//             <p class="percentage">
+//                 (10%)
+//             </p>
+//         </div>`;
+// });
